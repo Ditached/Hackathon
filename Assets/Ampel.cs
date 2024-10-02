5850c2f2ef7d96d889de7bf7c315a2d781eb7838
Seppi <seppi@ditached.de>
2024-10-02 14:53:15 +0200

    Stable du weißt

---
+++ Assets/Ampel.cs
@@ -0,0 +1,169 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using NWH.VehiclePhysics2;
+using Sirenix.OdinInspector;
+
+public enum AmpelState
+{
+    Green = 0,
+    Red = 1,
+    Yellow = 2
+}
+
+public class Ampel : MonoBehaviour
+{
+    [ReadOnly]
+    public bool tooLate;
+    public VehicleController vc;
+
+    [ReadOnly] public float carSpeed;
+    [ReadOnly] public float distanceToCar;
+    [ReadOnly] public float timeStayedInTargetVelocity = 0f;
+    public float calcWindow = 30f;
+    public float targetVelocity = 50f;
+    public float targetVelcoityWindow = 5f;
+    public float minTimeToStayInTargetVelocity = 5f;
+    public float distanceToSwitchToGreen;
+    
+    // G R Y 
+    // Green = 0
+    // Red = 1
+    // Yellow = 2
+    [Range(0,2)]
+    public int state = 0;
+    
+    public AmpelState ampelColor => (AmpelState) state;
+    
+    public List<Material> activeMaterials;
+    public List<Material> inactiveMaterials;
+
+    private MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        vc = FindObjectOfType<VehicleController>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        SetToRed();
+    }
+
+    private bool switchFlag;
+
+    public IEnumerator SwitchToGreen()
+    {
+        SetToYellow();
+        yield return new WaitForSeconds(1f);
+        SetToGreen();
+    }
+
+    public IEnumerator SwitchToGreenTooLate()
+    {
+        SetToRed();
+        yield return new WaitForSeconds(8f);
+        SetToYellow();
+        yield return new WaitForSeconds(1f);
+        SetToGreen();
+    }
+    
+    private bool enterFlag = false;
+
+    void Update()
+    {
+        //ApplyMaterial();
+        
+        var distance = Vector3.Distance(vc.transform.position, transform.position);
+        distanceToCar = distance;
+        
+        var speed = vc.Speed * 3.6f;
+        carSpeed = speed;
+
+        if (distance <= calcWindow)
+        {
+            if (!enterFlag)
+            {
+                enterFlag = true;
+                FindObjectOfType<MusicController>().currentSpeedLimit = targetVelocity;
+            }
+            
+            if (speed >= targetVelocity - targetVelcoityWindow && speed <= targetVelocity + targetVelcoityWindow)
+            {
+                timeStayedInTargetVelocity += Time.deltaTime;
+            }
+            else
+            {
+                if (timeStayedInTargetVelocity > 0f)
+                {
+                    timeStayedInTargetVelocity -= Time.deltaTime * 3f;
+                }
+            }
+            
+            if (distance <= distanceToSwitchToGreen && switchFlag == false)
+            {
+                switchFlag = true;
+                StopAllCoroutines();
+
+                if (timeStayedInTargetVelocity >= minTimeToStayInTargetVelocity)
+                {
+                    StartCoroutine(SwitchToGreen());
+                }
+                else
+                {
+                    FindObjectOfType<MusicController>().currentSpeedLimit = 0;
+                    tooLate = true;
+                    StartCoroutine(SwitchToGreenTooLate());
+                }
+            }
+        }
+        else
+        {
+            timeStayedInTargetVelocity = 0f;
+            switchFlag = false;
+            enterFlag = false;
+            tooLate = false;
+        }
+    }
+    
+    public void Set(AmpelState state)
+    {
+        this.state = (int) state;
+        ApplyMaterial();
+    }
+    
+    public void SetToGreen()
+    {
+        state = 0;
+        ApplyMaterial();
+    }
+    
+    public void SetToRed()
+    {
+        state = 1;
+        ApplyMaterial();
+    }
+    
+    public void SetToYellow()
+    {
+        state = 2;
+        ApplyMaterial();
+    }
+
+    private void ApplyMaterial()
+    {
+        Material[] materials = new Material[meshRenderer.sharedMaterials.Length];
+        
+        for (int i = 0; i < materials.Length; i++)
+        {
+            if (i == 0) // Assuming the first material should not change
+            {
+                materials[i] = meshRenderer.sharedMaterials[i];
+            }
+            else
+            {
+                int index = i - 1;
+                materials[i] = (state == index) ? activeMaterials[index] : inactiveMaterials[index];
+            }
+        }
+        
+        meshRenderer.sharedMaterials = materials;
+    }
+}
--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -29,74 +29,87 @@ public class MusicController : MonoBehaviour
 {
     public List<MusicLayer> musicLayers;
 
-    [Title("General")]
-    public Rigidbody rb;
+    [Title("General")] public Rigidbody rb;
     public VehicleController vehicleController;
 
 
     public float targetSpeedForAllMusicLayers = 50f;
     [ReadOnly] public float currentSpeed = 0f;
-    
-    [Title("Acceleration, Braking")]
-    public float maxAccl = 7f;
+
+    [Title("Acceleration, Braking")] public float maxAccl = 7f;
     public float maxAcclDeclVolume = 0.6f;
-    
+
     public bool enableAcclSound = false;
     public AudioSource accleleration;
     public AudioSource deceleration;
-    
+
     [ReadOnly] public Vector3 localAcceleration;
     [ReadOnly] public Vector3 localAccelerationLerped;
     public float acclLerpSpeed = 2f;
 
 
-    [Title("Too fast")] 
-    public float currentSpeedLimit = 50f;
+    [Title("Too fast / too slow")] public float currentSpeedLimit = 50f;
     [ReadOnly] public float speedOverLimit = 0f;
+    [ReadOnly] public float speedUnderLimit = 0f;
     public float minSpeedOverGreyArea = 5f;
     public AudioSource goingTooFast;
+    public AudioSource goingTooSlow;
     public float maxOverSpeed = 20f;
     public TMP_Text speedLimitText;
     public float maxTooFastVolume = 0.7f;
+    public float maxTooSlowVolume = 0.7f;
     public AudioMixer audioMixer;
     public float maxPitch = 20f;
-    
-     [Title("Blinker")]
-    public AudioSource blinkerLeft;
+    public float minSpeedForTooSlowToKickIn = 20f;
+
+    [Title("Blinker")] public AudioSource blinkerLeft;
     public AudioSource blinkerRight;
     public float fadeSpeed = 10f;
     public float maxBlinkerVolume = 0.6f;
 
     [ReadOnly] public bool blinkerLeftOn;
     [ReadOnly] public bool blinerRightOn;
-    
+
     public InputAction blinkerLeftAction;
     public InputAction blinkerRightAction;
 
-    [Title("Steering")] 
-    public bool enableSteeringSound = false;
+    [Title("Steering")] public bool enableSteeringSound = false;
     [ReadOnly] public float currentAngle;
     public float maxSteeringAngle = 14f;
     public float maxSteeringVolume = 0.6f;
     public float steeringLerpSpeed = 2f;
     public AudioSource steering;
+
     
 
     private void Awake()
     {
         blinkerLeftAction.performed += ctx => blinkerLeftOn = !blinkerLeftOn;
         blinkerLeftAction.Enable();
-        
+
         blinkerRightAction.performed += ctx => blinerRightOn = !blinerRightOn;
         blinkerRightAction.Enable();
     }
 
     private void Update()
     {
+        var amples = FindObjectsOfType<Ampel>();
+        
+        var nearestAmpel = amples[0];
+        
+        foreach (var ampel in amples)
+        {
+            if (Vector3.Distance(vehicleController.transform.position, ampel.transform.position) <
+                Vector3.Distance(vehicleController.transform.position, nearestAmpel.transform.position))
+            {
+                nearestAmpel = ampel;
+            }
+        }
+        
         currentSpeed = vehicleController.Speed * 3.6f;
-        
+
         var percent = currentSpeed / targetSpeedForAllMusicLayers;
-        
+
         foreach (var musicLayer in musicLayers)
         {
             musicLayer.Update(percent);
@@ -106,28 +119,44 @@ public class MusicController : MonoBehaviour
         if (enableSteeringSound)
         {
             currentAngle = vehicleController.steering.angle;
-            var steeringTargetVolume =  Mathf.InverseLerp(0, maxSteeringAngle, Mathf.Abs(currentAngle)) * maxSteeringVolume;
+            var steeringTargetVolume =
+                Mathf.InverseLerp(0, maxSteeringAngle, Mathf.Abs(currentAngle)) * maxSteeringVolume;
             steering.volume = Mathf.Lerp(steering.volume, steeringTargetVolume, Time.deltaTime * steeringLerpSpeed);
         }
 
+        
         speedOverLimit = Mathf.Max(0, currentSpeed - minSpeedOverGreyArea - currentSpeedLimit);
         goingTooFast.volume = Mathf.InverseLerp(0, maxOverSpeed, speedOverLimit) * maxTooFastVolume;
         speedLimitText.text = currentSpeedLimit.ToString("F0");
-        
+
+        speedUnderLimit = Mathf.Max(0, currentSpeedLimit - currentSpeed - minSpeedOverGreyArea);
+
+        if (currentSpeed < minSpeedForTooSlowToKickIn)
+        {
+            goingTooSlow.volume = 0f;
+        }
+        else
+        {
+            goingTooSlow.volume = Mathf.InverseLerp(0, maxOverSpeed, speedUnderLimit)  * maxTooSlowVolume;
+        }
+
+
         //Setting pith
         audioMixer.SetFloat("Pitch", 1f + Mathf.InverseLerp(0, maxOverSpeed, speedOverLimit) * maxPitch);
-   
+
         localAcceleration = vehicleController.LocalAcceleration;
-        
-        
-        localAccelerationLerped = Vector3.Lerp(localAccelerationLerped, localAcceleration, Time.deltaTime * acclLerpSpeed);
-        
-        if(enableAcclSound) accleleration.volume = Mathf.InverseLerp(0, maxAccl, localAccelerationLerped.z) * maxAcclDeclVolume;
+
+
+        localAccelerationLerped =
+            Vector3.Lerp(localAccelerationLerped, localAcceleration, Time.deltaTime * acclLerpSpeed);
+
+        if (enableAcclSound)
+            accleleration.volume = Mathf.InverseLerp(0, maxAccl, localAccelerationLerped.z) * maxAcclDeclVolume;
         deceleration.volume = Mathf.InverseLerp(0, maxAccl, -localAccelerationLerped.z) * maxAcclDeclVolume;
 
         var blinkerLeftVolume = blinkerLeftOn ? maxBlinkerVolume : 0f;
-        var blinkerRightVolume = blinerRightOn? maxBlinkerVolume : 0f;
-        
+        var blinkerRightVolume = blinerRightOn ? maxBlinkerVolume : 0f;
+
         blinkerLeft.volume = Mathf.Lerp(blinkerLeft.volume, blinkerLeftVolume, Time.deltaTime * fadeSpeed);
         blinkerRight.volume = Mathf.Lerp(blinkerRight.volume, blinkerRightVolume, Time.deltaTime * fadeSpeed);
     }
